25d9cab9bb4a6812f0063f087d07deee9992d195
Want-EyeTea <Want_EyeTea@protonmail.com>
2025-11-26 15:17:44 -0600

    Update: terminal_setup.sh

--- dot-files/terminal_setup.sh
+++ dot-files/terminal_setup.sh
@@ -1,11 +1,34 @@
 # List of Apps
 installs=(
-  "extra/git",
-  "nvim",
-  "starship",
-  "rg"
-  "fd"
-  )
+  "extra/bat",
+  "extra/bind",
+  "extra/docker",
+  "extra/eza",
+  "extra/fd",
+  "extra/fzf",
+  "core/less",
+  "extra/lf",
+  "extra/luarocks",
+  "extra/man-pages-es",
+  "extra/neovim",
+  "extra/nodejs",
+  "extra/npm",
+  "core/openssh",
+  "core/python",
+  "extra/python-openpyxl",
+  "extra/python-pandas",
+  "extra/ranger",
+  "extra/ripgrep",
+  "extra/starship",
+  "extra/unzip",
+  "extra/vim",
+  "extra/wget",
+  "extra/whois",
+  "extra/zellij",
+  "core/net-tools"
+)
 
-for $i in ${installs[@]}; do
+for i in "${installs[@]}"; do
   sudo pacman -Sy $i; done
+
+echo -e "\nAll packages installed!"
